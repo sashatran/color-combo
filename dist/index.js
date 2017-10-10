bb8e13e28abd6322aa85774c88ed544d02ea7c67
sashatran <shasha26789@gmail.com>
2017-10-10 15:21:05 -0700

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,129 @@
+// convert rgb to hex
+function componentFromStr(numStr, percent) {
+  var num = Math.max(0, parseInt(numStr, 10));
+  return percent ?
+  Math.floor(255 * Math.min(100, num) / 100) : Math.min(255, num);
+}
+
+function rgbToHex(rgb) {
+  var rgbRegex = /^rgb\(\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*\)$/;
+  var result, r, g, b, hex = "";
+  if ( (result = rgbRegex.exec(rgb)) ) {
+    r = componentFromStr(result[1], result[2]);
+    g = componentFromStr(result[3], result[4]);
+    b = componentFromStr(result[5], result[6]);
+
+    hex = "#" + (0x1000000 + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+  return hex;
+}
+
+function colorData(favorites){
+  let set = favorites.map(function(item,index){
+    let bgColor = "rgb(" + item.colors[0].toString() + ")";
+    let textColor = "rgb(" + item.colors[1].toString() + ")";
+    let box = document.createElement("div");
+    box.style.backgroundColor = bgColor;
+    box.style.color = textColor;
+    box.className = "box";
+    return box;
+  });
+
+  set.forEach(function(box, index){
+    $(".container").append(box);
+    animate(box,index);
+  });
+};
+
+
+// retrieve data from JSON
+function getData(){
+  $.ajax({
+    method: "GET",
+    url: "./data.json",
+    success: function(data){
+      colorData(data.favorites);
+    },
+    error: function(err){
+      console.log(err);
+    }
+  });
+}
+
+// Animate in the boxes
+function animate(box, index){
+  let delay = index * 200;
+  setTimeout(function(){
+    $(box).addClass("enter")
+  }, delay)
+}
+
+function copy(e){
+  let bgColor = rgbToHex(document.documentElement.style.getPropertyValue("--bgColor"));
+  let textColor = rgbToHex(document.documentElement.style.getPropertyValue("--textColor"));
+  let copyStr = "background-color: " + bgColor + ";" + "\n color: " + textColor + ";";
+
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val(copyStr).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+function handleClick(box){
+  console.log(box);
+  $("body").addClass("modal-open");
+  let bgColor = box.style.backgroundColor;
+  let textColor = box.style.color;
+
+  document.documentElement.style.setProperty("--bgColor", bgColor);
+  document.documentElement.style.setProperty("--textColor", textColor);
+
+  $(".modal").css("transform", "translateX(0%)");
+}
+
+function closeModal(modal){
+  let parent =  $(modal).parent().parent()[0].id;
+  if(parent === "about") {
+    $("#"+parent).css("transform", "translateY(-200%)");
+  }
+
+  if(parent === "box") {
+    $(".modal").css("transform", "translateX(-100%)");
+    $(".copy").removeClass("copied");
+  }
+}
+
+function tweetMe(tweet){
+  let tweetContent = "Check out this color collection by @sa_sha26";
+  $(tweet).attr("href", "https://twitter.com/intent/tweet?text="+ tweetContent);
+}
+
+$(document).ready(function(){
+
+  getData();
+
+  $(document).on("click", ".box", function(){
+    handleClick(this);
+  });
+
+  $(".close").click(function(){
+    $("body").removeClass("modal-open");
+   closeModal(this);
+ });
+
+  $("#tweet").click(function(){
+    tweetMe(this);
+  });
+
+  $(".copy").click(function(e){
+    copy(e);
+    $(this).addClass("copied");
+  });
+
+  $(".about").click(function(){
+    $("body").addClass("modal-open");
+    $("#about").css("transform", "translateY(0%)");
+  });
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function(){
+	gulp.src('./scss/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./dist'))
+	.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('scripts', function(){
+	gulp.src('./js/*.js').
+	pipe(gulp.dest('./dist'));
+})
+
+gulp.task('serve', function(){
+	browserSync.init({
+		server: {
+			baseDir: './dist'
+		}
+	});
+
+	gulp.watch('./scss/*.scss', ['styles']);
+	gulp.watch('./js/*.js', ['scripts']);
+	gulp.watch('./**/*.html').on('change',browserSync.reload);
+	gulp.watch('./**/*.js').on('change',browserSync.reload);
+});
+
+
+gulp.task('default', ['styles','scripts','serve']);
